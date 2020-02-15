4572ecb74e05e3ed952b6342270eac69ec5cb34b
Haitham Abdel-Salam <Haithamhany7@gmail.com>
2020-02-15 16:35:15 -0500

    Add project files.

---
+++ Socket Server/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Socket_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Server server = new Server();
+            server.ExecuteServer();
+        }
+    }
+}
