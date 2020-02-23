aee6e5678031f40fe1ac7f9da5a5c5f1f3bc6e36
Haitham Abdel-Salam <Haithamhany7@gmail.com>
2020-02-23 12:09:58 -0500

    fixing server issues

--- Socket Server/Program.cs
+++ Socket Server/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Socket_Server
 {
@@ -6,8 +12,53 @@ namespace Socket_Server
     {
         static void Main(string[] args)
         {
-            Server server = new Server();
-            server.ExecuteServer();
+            #region old server code
+            //IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            //TcpListener server = new TcpListener(ip, 8080);
+            //TcpClient client = default(TcpClient);
+            //List<TcpClient> clients = new List<TcpClient>();
+
+            //try
+            //{
+            //    server.Start();
+            //    Console.WriteLine("Server started ...");
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
+            //    Console.Read();
+            //}
+
+            //while(true)
+            //{
+            //    client = server.AcceptTcpClient();
+
+            //    if(!clients.Contains(client))
+            //        clients.Add(client);
+
+            //    byte[] recivedBuffer = new byte[1000];
+            //    NetworkStream stream = client.GetStream();
+
+            //    stream.Read(recivedBuffer,0 , recivedBuffer.Length);
+
+            //    //string msg = Encoding.ASCII.GetString(recivedBuffer, 0, recivedBuffer.Length);
+
+            //    StringBuilder msg = new StringBuilder();
+            //    foreach (byte b in recivedBuffer)
+            //    {
+            //        //59 is the ascii code for the Semicolon 00 is for null
+            //        if (b.Equals(00)) 
+            //            break;
+            //        else
+            //            msg.Append(Convert.ToChar(b).ToString());
+            //    }
+
+            //    Console.WriteLine(msg.ToString());
+
+            //}
+            #endregion
+
+            Server.ExecuteServer();
         }
     }
 }
